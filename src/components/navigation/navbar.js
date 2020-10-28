dd5953071ab17438b07cc7c98f0f90c4e97c2106
Kyle Phillips <kphillips375@yahoo.com>
2020-10-27 20:24:45 -0400

    navbar

---
+++ src/components/navigation/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
+const Nav = () => {
+  
+    return (
+        <div className="">
+            <nav className="main-nav ">
+                <div className="main-nav-logo">
+                    <h1><NavLink to="/">Tandem Quiz</NavLink></h1>
+                </div>
+                <nav className="main-nav-items">
+                    <ul>
+                        <li><NavLink to="/start">Start</NavLink></li>
+                        <li><NavLink to="/blogs">Reach Out</NavLink></li>
+                            <React.Fragment>
+                                <li><NavLink to="/highlights">Highlighted</NavLink></li>
+                            </React.Fragment>
+                    </ul>
+                </nav>
+            </nav>
+            <div className="hrDiv">
+            <hr className="hr"  />
+            </div>
+        </div>
+    )
+ }
+export default Nav;
