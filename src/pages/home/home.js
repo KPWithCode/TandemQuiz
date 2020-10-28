28f8b07b93977350fa7057529d8866c336d64f91
Kyle Phillips <kphillips375@yahoo.com>
2020-10-28 00:56:47 -0400

    home screen

---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+
+    return (
+        <div className="background">
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default Home
