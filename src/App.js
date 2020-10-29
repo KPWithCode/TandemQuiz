60dd1bf43a4f7a7bf9edae7da76f9dc7f53c0c3f
Kyle Phillips <kphillips375@yahoo.com>
2020-10-28 22:38:38 -0400

    Introduced all routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/navigation/navbar';
 import Home from './pages/home/home';
 import Start from './pages/quiz/quiz';
+import About from './pages/about/about';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/start" component={Start} />
+          <Route exact path="/about" component={About} />
         </Switch>
       </>
     </Router>
