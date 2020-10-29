06b4ab7c180bffdcd71a1120b9a09b670e84d757
Kyle Phillips <kphillips375@yahoo.com>
2020-10-29 01:02:13 -0400

    State, and functions complete.. component displaying correctyle

--- src/pages/quiz/quiz.js
+++ src/pages/quiz/quiz.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import './quiz.css'
 import QuizData from '../../data/Apprentice_TandemFor400_Data.json';
+import { motion } from 'framer-motion'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure()
 
 
 const Quiz = () => {
@@ -8,40 +12,97 @@ const Quiz = () => {
     const [currQuestion, setQuestion] = useState(0)
     const [score, setScore] = useState(0)
     const [answers, setAnswers] = useState([])
+    const [show, setShow] = useState(false)
 
+
+     function correct(){
+        toast.success('Correct!', { position: toast.POSITION.TOP_RIGHT, autoClose: 3000 })
+    }
+     function incorrect(){
+        toast.error('Incorrect! Maybe next time', { position: toast.POSITION.TOP_LEFT, autoClose: 3000 })
+    }
     const handleQuestions = () => {
+        incorrect()
         const nextQuestion = currQuestion + 1
-        if (nextQuestion < QuizData.question.length) {
+        if (nextQuestion < 10) {
             setQuestion(nextQuestion)
         } else {
-            alert('thats it for the quiz')
+            setShow(true)
         }
     }
     const handleCorrect = () => {
+        correct()
+        const nextQuestion = currQuestion + 1
+        if (nextQuestion < 10) {
+            setQuestion(nextQuestion)
+        } else {
+            setShow(true)
+        }
         setScore(score + 1)
-        handleQuestions()
     }
+
+    const shuffleQuestions = (array) => {
+        let length = array.length;
+
+        while (0 !== length) {
+            let random = Math.floor(Math.random() * length);
+            length -= 1;
+            let placement = array[length];
+            array[length] = array[random]
+            array[random] = placement
+        }
+        return array
+    }
+
+    function questionList(arr) {
+        arr = shuffleQuestions(arr)
+        let questionArray = []
+        for (let i = 0; i < arr.length; i++) {
+            let questions = arr[i]
+            questionArray.push(questions)
+
+            if (!questionArray.includes(questions) && questionArray.length < 10) {
+                questionArray.push(questions)
+            }
+        }
+        return questionArray
+    }
+    const quizList = questionList(QuizData)
 
     return (
         <div className="quiz-background">
-            <div className="quiz-data">
-                <div className="quiz-question">
-                    {QuizData[currQuestion].question}
-                </div>
-                <div className="quiz-choices">
-                    {QuizData[currQuestion].incorrect.map((wrong) => {
-                        return <div className="button-div"><button>{wrong}</button></div>
-                    })}
-                    {/* <button>{QuizData[currQuestion].correct}</button> */}
-                </div>
-            </div>
+            <motion.div
+                initial={{ scale: 0 }}
+                animate={{ rotate: 180, scale: -1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20
+                }}
+                className="quiz-data">
+                <h5> Question {currQuestion + 1} / {10}</h5>
+
+                {show ? <div><div> You got {score} out of 10 right</div><button onClick={() => window.location.replace('/start')} className="button">Play Again</button></div> :
+                    <>
+                        <div className="quiz-question">
+                            {quizList[currQuestion].question}
+                        </div>
+                        <div className="quiz-choices">
+                            {quizList[currQuestion].incorrect.map((wrong) => {
+                                return <div className="button-div"><button className="button" onClick={handleQuestions}>{wrong}</button></div>
+                            })}
+                            <div className="button-div"><button className="button" onClick={handleCorrect}>{quizList[currQuestion].correct}</button></div>
+                        </div>
+                    </>
+                }
+            </motion.div>
         </div>
     )
 }
 
 export default Quiz
 
-//  {QuizData.map((quiz, index) => {
+//  {quizList.map((quiz, index) => {
     // return (
     //     <div key={index}>
     //             <ol>{quiz.question}</ol>
