655a3526f50b249859b636a04af0aa0dc1f0e316
Kyle Phillips <kphillips375@yahoo.com>
2020-10-28 22:40:02 -0400

    Home page updated

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './home.css'
-
+import { motion } from 'framer-motion';
 const Home = () => {
 
     return (
         <div className="background">
-            <h1>Welcome</h1>
+            <motion.div 
+            initial={{ scale: 0 }}
+            animate={{ rotate: 180, scale: -1 }}
+            transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20
+            }}
+            className="welcome-div">
+            <h1 className="title">Tandem Quiz</h1> <br />
+            <h1 className="subtitle"> Welcome Aboard</h1>
+            </motion.div>
         </div>
     )
 }
