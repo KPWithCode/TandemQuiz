4ec077b354242c31b177cee6a5a5a534214f5836
Kyle Phillips <kphillips375@yahoo.com>
2020-11-01 12:20:39 -0500

    toast error and success messages

--- src/pages/quiz/quiz.js
+++ src/pages/quiz/quiz.js
@@ -15,12 +15,12 @@ const Quiz = () => {
     const [show, setShow] = useState(false)
 
     function correct() {
-        toast.success('Correct!', { position: toast.POSITION.TOP_RIGHT, autoClose: 3000 })
+        toast.success('Correct!', { position: toast.POSITION.TOP_RIGHT, autoClose: 2500 })
     }
     function incorrect() {
-        toast.error('Incorrect! Maybe next time', { position: toast.POSITION.TOP_LEFT, autoClose: 3000 })
+        toast.error('Incorrect! Maybe next time', { position: toast.POSITION.TOP_LEFT, autoClose: 2500 })
     }
-    
+
     const handleQuestions = () => {
         incorrect()
         const nextQuestion = currQuestion + 1
@@ -40,8 +40,8 @@ const Quiz = () => {
         }
         setScore(score + 1)
     }
-    
     const quizList = questionList(QuizData)
+
 
     return (
         <div className="quiz-background">
@@ -65,24 +65,24 @@ const Quiz = () => {
                             {quizList[currQuestion].question}
                         </div>
                         <div className="quiz-choices">
-                            {quizList[currQuestion].incorrect.map((wrong) => {
-                                return <motion.div 
+                            {quizList[currQuestion].incorrect.map((wrong, index) => {
+                                return <motion.div
+                                    whileHover={{
+                                        scale: 1.1,
+                                        transition: {
+                                            duration: .2
+                                        }
+                                    }}
+                                    className="button-div"><button className="button" onClick={handleQuestions}>{wrong}</button></motion.div>
+                            })}
+                            <motion.div
                                 whileHover={{
                                     scale: 1.1,
                                     transition: {
                                         duration: .2
                                     }
                                 }}
-                                className="button-div"><button className="button" onClick={handleQuestions}>{wrong}</button></motion.div>
-                            })}
-                            <motion.div 
-                            whileHover={{
-                                scale: 1.1,
-                                transition: {
-                                    duration: .2
-                                }
-                            }}
-                            className="button-div"><button className="button" onClick={handleCorrect}>{quizList[currQuestion].correct}</button></motion.div>
+                                className="button-div"><button className="button" onClick={handleCorrect}>{quizList[currQuestion].correct}</button></motion.div>
                         </div>
                     </>
                 }
